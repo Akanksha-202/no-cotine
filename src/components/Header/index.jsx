80195e95a33028aaa7537241e5fad98d92573e2d
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2024-01-30 14:36:51 +0000

    Issue resolved

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,42 +6,16 @@ const Header = () => (
   <header className='home-header'>
     <nav className="nav bd-container">
       <a href="/" className="nav__logo">
-      Nocotine 🚭;
+        Nocotine 🚭
       </a>
       <div className="nav__menu" id="nav-menu">
         <ul className="nav__list">
-        <li className="nav__item">
+          <li className="nav__item">
             <a href="#about" className="nav__link ">
               About
             </a>
           </li>
-          {/* 
-          <li className="nav__item">
-            <a href="#theme" className="nav__link ">
-              Theme
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#events" className="nav__link">
-              Events
-            </a>
-          </li> */}
-          {/* <li class="nav__item"><a href="#timeline" class="nav__link">Timeline</a></li> */}
-          <li className="nav__item">
-            <a href="#features" className="nav__link">
-             Features
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="#team" className="nav__link" >
-              Team
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="/accounts" className="nav__link">
-              Login
-            </a>
-          </li>
+          {/* Other menu items */}
           <li>
             <i className="bx bx-toggle-left change-theme" id="theme-button" />
           </li>
@@ -52,14 +26,17 @@ const Header = () => (
       </div>
     </nav>
 
-  <header className='home-header mt-16'>
-    <Navbar />
-    <h1>
-     Reading Area
-    </h1>
-    <p>
-     Explore a curated compilation of resources dedicated to the journey of quitting smoking <br></br>and discover strategies for maintaining your physical and mental well-being. <br></br>Embark on a path towards a smoke-free life, embracing health and authenticity. Quit smoking, stay well, be you!
-    </p>
+    {/* Second opening tag for header */}
+    <header className='home-header mt-16'>
+      <Navbar />
+      <h1>Reading Area</h1>
+      <p>
+        Explore a curated compilation of resources dedicated to the journey of quitting smoking <br></br>
+        and discover strategies for maintaining your physical and mental well-being. <br></br>
+        Embark on a path towards a smoke-free life, embracing health and authenticity. Quit smoking, stay well, be you!
+      </p>
+    </header>
+    {/* Closing tag for the second header */}
   </header>
 );
 
